9fd2c45a6f637b0de69e79fef63579e4d2c4100e
Scott Carleton <scott@artsicle.com>
2017-07-27 15:55:34 -0400

    Install script for Crystal

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Hello World
 
+[![CircleCI](https://circleci.com/gh/ScotterC/artsycle.svg?style=svg)](https://circleci.com/gh/ScotterC/artsycle)
+
 ## Run Tests
 
 `make test`
---
+++ scripts/crystal_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export CRYSTAL_VERSION=0.21.1
+export CRYSTAL_URL="https://github.com/crystal-lang/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz"
+
+if [[ ! -e "${HOME}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
+  mkdir "${HOME}/.crystal"
+  wget "${CRYSTAL_URL}" -O "${HOME}/crystal.tar.gz" &&\
+  tar -zxvf "${HOME}/crystal.tar.gz" -C "${HOME}/.crystal" --strip-components=1
+fi
