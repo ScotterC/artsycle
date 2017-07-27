38a97449d5d2209c139e5f3adf0a79c99f349402
Scott Carleton <scott@artsicle.com>
2017-07-27 16:21:34 -0400

    New Home for working directory

--- scripts/crystal_install.sh
+++ scripts/crystal_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export CRYSTAL_VERSION=0.21.1
 export CRYSTAL_URL="https://github.com/crystal-lang/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz"
+export HOME="~/artsycle"
 
-# if [[ ! -e "${CIRCLE_WORKING_DIRECTORY}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
-  mkdir ".crystal"
-  wget "${CRYSTAL_URL}" -O "crystal.tar.gz" &&\
-  tar -zxvf "crystal.tar.gz" -C ".crystal" --strip-components=1
-# fi
+if [[ ! -e "${HOME}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
+  mkdir "${HOME}/.crystal"
+  wget "${CRYSTAL_URL}" -O "${HOME}/crystal.tar.gz" &&\
+  tar -zxvf "${HOME}/crystal.tar.gz" -C "${HOME}/.crystal" --strip-components=1
+fi
