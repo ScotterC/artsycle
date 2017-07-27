d4511c7f513d7bdd4750b1ad81077bf67fda2ef9
Scott Carleton <scott@artsicle.com>
2017-07-27 16:01:03 -0400

    Use the working directory

--- scripts/crystal_install.sh
+++ scripts/crystal_install.sh
@@ -2,8 +2,8 @@
 export CRYSTAL_VERSION=0.21.1
 export CRYSTAL_URL="https://github.com/crystal-lang/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz"
 
-if [[ ! -e "${HOME}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
-  mkdir "${HOME}/.crystal"
-  wget "${CRYSTAL_URL}" -O "${HOME}/crystal.tar.gz" &&\
-  tar -zxvf "${HOME}/crystal.tar.gz" -C "${HOME}/.crystal" --strip-components=1
+if [[ ! -e "${CIRCLE_WORKING_DIRECTORY}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
+  mkdir "${CIRCLE_WORKING_DIRECTORY}/.crystal"
+  wget "${CRYSTAL_URL}" -O "${CIRCLE_WORKING_DIRECTORY}/crystal.tar.gz" &&\
+  tar -zxvf "${CIRCLE_WORKING_DIRECTORY}/crystal.tar.gz" -C "${CIRCLE_WORKING_DIRECTORY}/.crystal" --strip-components=1
 fi
