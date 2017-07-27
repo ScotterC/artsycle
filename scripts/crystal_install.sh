204b9eff5f5c6b70fa3c677dc5b175c1d6bcdb25
Scott Carleton <scott@artsicle.com>
2017-07-27 16:22:41 -0400

    env variables aren't working

--- scripts/crystal_install.sh
+++ scripts/crystal_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export CRYSTAL_VERSION=0.21.1
 export CRYSTAL_URL="https://github.com/crystal-lang/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz"
-export HOME="~/artsycle"
 
-if [[ ! -e "${HOME}/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
-  mkdir "${HOME}/.crystal"
-  wget "${CRYSTAL_URL}" -O "${HOME}/crystal.tar.gz" &&\
-  tar -zxvf "${HOME}/crystal.tar.gz" -C "${HOME}/.crystal" --strip-components=1
+if [[ ! -e "~/artsycle/.crystal/bin/crystal" || $(crystal --version | grep -P -o '\d+\.\d+\.\d+') != "${CRYSTAL_VERSION}" ]]; then
+  mkdir "~/artsycle/.crystal"
+  wget "${CRYSTAL_URL}" -O "~/artsycle/crystal.tar.gz" &&\
+  tar -zxvf "~/artsycle/crystal.tar.gz" -C "~/artsycle/.crystal" --strip-components=1
 fi
